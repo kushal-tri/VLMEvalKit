d42d41388b2b61a3656286ca211aab0ec880c7f6
Haodong Duan <dhd@pku.edu.cn>
2024-11-21 14:39:31 +0800

    [Improvement] Launch Evaluation w. Config (#610)
    * [Improvement] Support Launching Eval w. --config
    
    * Fix Inference Scripts
    
    * [Fix] Fix build_from_config issues
    
    * improve run.py -h help message
    
    * Refactor Doc
    
    * add doc for the config system
    
    * [Fix] aria on mvbench and llava_onevision in video benchmark
    
    * [Fix] Idefics on mlvu, tempcompass and mvbench
    
    * [Model] Support SmolVLM (#615)
    
    * adding smolvlm
    
    * changing model path
    
    * fix
    
    * pre-commit fixes
    
    * [Fix] Enable --reuse with resume from original pkl files with same commit id (#613)
    
    * [Fix] Enable --reuse with resume from original pkl files with same commit id
    
    * only transfer target dataset .pkl file
    
    * update reuse logic
    
    ---------
    
    Co-authored-by: kennymckormick <dhd.efz@gmail.com>
    
    * [Benchmark] Support MM-Math (#618)
    
    * [Benchmark] Support MM-Math Evaluation
    
    * update README
    
    * update README
    
    * [Benchmark] Measuring Quantitative Spatial Reasoning with the Q-Spatial Bench🔥 (#569)
    
    * add Q-Spatial Bench
    
    * perform pre-commit
    
    * use spatialprompt_single
    
    ---------
    
    Co-authored-by: Haodong Duan <dhd@pku.edu.cn>
    
    * update README
    
    * [Improvement] Support Launching Eval w. --config
    
    ---------
    
    Co-authored-by: FangXinyu-0913 <fangxinyutju202009@126.com>
    Co-authored-by: Miquel Farré <miquel.farre@gmail.com>
    Co-authored-by: Andrew <andrewliao11@gmail.com>

--- vlmeval/inference_video.py
+++ vlmeval/inference_video.py
@@ -18,11 +18,11 @@ def parse_args():
 
 
 # Only API model is accepted
-def infer_data_api(work_dir, model_name, dataset, nframe=8, pack=False, samples_dict={}, api_nproc=4, fps=-1):
+def infer_data_api(model, work_dir, model_name, dataset, nframe=8, pack=False, samples_dict={}, api_nproc=4, fps=-1):
     rank, world_size = get_rank_and_world_size()
     assert rank == 0 and world_size == 1
     dataset_name = dataset.dataset_name
-    model = supported_VLM[model_name]() if isinstance(model_name, str) else model_name
+    model = supported_VLM[model_name]() if isinstance(model, str) else model
     assert getattr(model, 'is_api', False)
 
     indices = list(samples_dict.keys())
@@ -49,7 +49,8 @@ def infer_data_api(work_dir, model_name, dataset, nframe=8, pack=False, samples_
     return res
 
 
-def infer_data(model_name, model_path, work_dir, dataset, out_file, nframe=8, pack=False, verbose=False, api_nproc=4):
+def infer_data(model, model_name, model_path, work_dir, dataset, out_file, nframe=8,
+               pack=False, verbose=False, api_nproc=4, fps=-1):
     res = load(out_file) if osp.exists(out_file) else {}
     rank, world_size = get_rank_and_world_size()
     dataset_name = dataset.dataset_name
@@ -60,18 +61,19 @@ def infer_data(model_name, model_path, work_dir, dataset, out_file, nframe=8, pa
 
     sample_indices_sub = sample_indices[rank::world_size]
     if np.all([idx in res for idx in sample_indices_sub]):
-        return model_name
+        return model
     sample_indices_subrem = [x for x in sample_indices_sub if x not in res]
 
     if model_path is None:
-        model = supported_VLM[model_name]() if isinstance(model_name, str) else model_name
+        model = supported_VLM[model_name]() if isinstance(model, str) else model
     else:
-        model = supported_VLM[model_name](model_path) if isinstance(model_name, str) else model_name
+        model = supported_VLM[model_name](model_path) if isinstance(model, str) else model
 
     is_api = getattr(model, 'is_api', False)
     if is_api:
         assert world_size == 1
         supp = infer_data_api(
+            model=model,
             work_dir=work_dir,
             model_name=model_name,
             dataset=dataset,
@@ -84,7 +86,7 @@ def infer_data(model_name, model_path, work_dir, dataset, out_file, nframe=8, pa
             assert k in supp
         res.update(supp)
         dump(res, out_file)
-        return model_name
+        return model
 
     for i, idx in tqdm(enumerate(sample_indices_subrem)):
         if idx in res:
@@ -162,7 +164,7 @@ def infer_data_job_video(
         result_file = result_file.replace('.xlsx', f'_{subtitle_str}.xlsx')
     # Dump Predictions to Prev File if result file exists
     if osp.exists(result_file):
-        return model_name
+        return model
 
     if nframe > 0:
         tmpl = osp.join(work_dir, '{}' + f'{world_size}_{dataset_name}_{nframe}frame_{packstr}.pkl')
@@ -175,8 +177,9 @@ def infer_data_job_video(
     out_file = tmpl.format(rank)
 
     model = infer_data(
-        model,
-        model_path,
+        model=model,
+        model_name=model_name,
+        model_path=model_path,
         work_dir=work_dir,
         dataset=dataset,
         nframe=nframe,
